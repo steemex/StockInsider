19b917d168adb04f09279ca7b797810bdad8d6fe
Andrey Sinyagin <steemex@ya.ru>
2018-07-26 12:30:10 +0300

    Initial Commit

---
+++ StockInsider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockInsider
+//
+//  Created by Andrey Sinyagin on 26/07/2018.
+//  Copyright © 2018 Andrey Sinyagin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
